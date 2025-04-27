67596cd4b8590407c556a25f45663dc9cbc8ffda
berdichevskiia <116463190+berdichevskiia@users.noreply.github.com>
2025-04-27 20:28:21 +0300

    upd pr 1 and pr2

--- students/K3339/Berdichevskii_Artem/lr_1/Practice_1/main.py
+++ students/K3339/Berdichevskii_Artem/lr_1/Practice_1/main.py
@@ -1,108 +1,135 @@
-import datetime
+from typing import List
 
 from fastapi import FastAPI
+from typing_extensions import TypedDict
 
-from lab1.prac1.models import Task, TaskWithLinks, Message, Data, Sprint
-
-temp_bd = [
-    {
-        "id": 1,
-        "summary": "Do something",
-        "priority": "trivial",
-        "description": "Do something",
-        "planned_end_at": datetime.datetime(hour=10, minute=0, second=0, day=10, month=1, year=2024),
-        "status": "open",
-        "created_at": datetime.datetime(hour=10, minute=0, second=0, day=5, month=1, year=2024),
-        "updated_at": datetime.datetime(hour=10, minute=0, second=0, day=5, month=1, year=2024),
-        "sprint": {
-            "id": 1,
-            "title": "Sprint1",
-            "start_at": datetime.datetime(hour=10, minute=0, second=0, day=1, month=1, year=2024),
-            "end_at": datetime.datetime(hour=10, minute=0, second=0, day=14, month=1, year=2024),
-        },
-        "linked_tasks": [
-            {
-                "task": {
-                    "id": 2,
-                    "summary": "Do something other",
-                    "priority": "major",
-                    "description": "Do something other",
-                    "planned_end_at": datetime.datetime(hour=10, minute=0, second=0, day=10, month=1, year=2024),
-                    "status": "in_progress",
-                    "created_at": datetime.datetime(hour=10, minute=0, second=0, day=5, month=1, year=2024),
-                    "updated_at": datetime.datetime(hour=10, minute=0, second=0, day=5, month=1, year=2024),
-                    "sprint": {
-                        "id": 2,
-                        "title": "Sprint2",
-                        "start_at": datetime.datetime(hour=10, minute=0, second=0, day=1, month=1, year=2024),
-                        "end_at": datetime.datetime(hour=10, minute=0, second=0, day=14, month=1, year=2024),
-                    },
-                },
-                "status": "is_blocked_by",
-            }
-        ],
-    },
-    {
-        "id": 2,
-        "summary": "Do something other",
-        "priority": "major",
-        "description": "Do something other",
-        "planned_end_at": datetime.datetime(hour=10, minute=0, second=0, day=10, month=1, year=2024),
-        "status": "in_progress",
-        "created_at": datetime.datetime(hour=10, minute=0, second=0, day=5, month=1, year=2024),
-        "updated_at": datetime.datetime(hour=10, minute=0, second=0, day=5, month=1, year=2024),
-        "sprint": {
-            "id": 2,
-            "title": "Sprint2",
-            "start_at": datetime.datetime(hour=10, minute=0, second=0, day=1, month=1, year=2024),
-            "end_at": datetime.datetime(hour=10, minute=0, second=0, day=14, month=1, year=2024),
-        },
-        "linked_tasks": [],
-    },
-]
+from models import Warrior, Profession
 
 app = FastAPI()
 
+temp_bd_warriors = [
+    {
+        "id": 1,
+        "race": "director",
+        "name": "Мартынов Дмитрий",
+        "level": 12,
+        "profession": {
+            "id": 1,
+            "title": "Влиятельный человек",
+            "description": "Эксперт по всем вопросам"
+        },
+        "skills":
+            [{
+                "id": 1,
+                "name": "Купле-продажа компрессоров",
+                "description": ""
 
-@app.get("/tasks")
-def get_tasks() -> list[TaskWithLinks]:
-    return [TaskWithLinks(**task) for task in temp_bd]
+            },
+                {
+                    "id": 2,
+                    "name": "Оценка имущества",
+                    "description": ""
+
+                }]
+    },
+    {
+        "id": 2,
+        "race": "worker",
+        "name": "Андрей Косякин",
+        "level": 12,
+        "profession": {
+            "id": 1,
+            "title": "Дельфист-гребец",
+            "description": "Уважаемый сотрудник"
+        },
+        "skills": []
+    },
+]
+
+temp_bd_professions = [
+    {
+        "id": 1,
+        "title": "Влиятельный человек",
+        "description": "Эксперт по всем вопросам"
+    },
+    {
+            "id": 2,
+            "title": "Дельфист-гребец",
+            "description": "Уважаемый сотрудник"
+    }
+]
+
+@app.get("/")
+def hello():
+    return "Hello, [username]!"
 
 
-@app.get("/tasks/{task_id}")
-def get_task(task_id: int) -> TaskWithLinks | None:
-    for task in temp_bd:
-        if task.get("id") == task_id:
-            return TaskWithLinks(**task)
-    else:
-        return None
+@app.get("/warriors_list")
+def warriors_list() -> List[Warrior]:
+    return temp_bd_warriors
 
 
-@app.post("/tasks")
-def add_task(task: Task, sprint: Sprint | None = None) -> Data:
-    created_task = task.model_dump()
-
-    if sprint:
-        created_task["sprint"] = sprint.model_dump()
-
-    temp_bd.append(created_task)
-    return Data(status=200, data=Task(**created_task))
+@app.get("/warrior/{warrior_id}")
+def warriors_get(warrior_id: int) -> List[Warrior]:
+    return [warrior for warrior in temp_bd_warriors if warrior.get("id") == warrior_id]
 
 
-@app.delete("/tasks/{task_id}", status_code=201)
-def delete_task(task_id: int) -> Message:
-    for i, task in enumerate(temp_bd):
-        if task.get("id") == task_id:
-            temp_bd.pop(i)
+@app.post("/warrior")
+def warriors_create(warrior: Warrior) -> TypedDict('Response', {"status": int, "data": Warrior}):
+    warrior_to_append = warrior.model_dump()
+    temp_bd_warriors.append(warrior_to_append)
+    return {"status": 200, "data": warrior}
+
+
+@app.delete("/warrior/delete{warrior_id}")
+def warrior_delete(warrior_id: int):
+    for i, warrior in enumerate(temp_bd_warriors):
+        if warrior.get("id") == warrior_id:
+            temp_bd_warriors.pop(i)
             break
+    return {"status": 201, "message": "deleted"}
 
-    return Message(status=201, message="deleted")
+
+@app.put("/warrior{warrior_id}")
+def warrior_update(warrior_id: int, warrior: Warrior) -> List[Warrior]:
+    for war in temp_bd_warriors:
+        if war.get("id") == warrior_id:
+            warrior_to_append = warrior.model_dump()
+            temp_bd_warriors.remove(war)
+            temp_bd_warriors.append(warrior_to_append)
+    return temp_bd_warriors
+
+@app.get("/professions_list")
+def professions_list() -> List[Profession]:
+    return temp_bd_professions
 
 
-@app.put("/tasks/{task_id}")
-def update_task(task_id: int, task_data: Task) -> Task:
-    for task in temp_bd:
-        if task.get("id") == task_id:
-            temp_bd.remove(task)
-            temp_bd.append(task_data.model_dump())
-            return task_data
+@app.get("/professions/{profession_id}")
+def professions_get(profession_id: int) -> List[Profession]:
+    return [profession for profession in temp_bd_professions if profession.get("id") == profession_id]
+
+
+@app.post("/profession")
+def profession_create(profession: Profession) -> TypedDict('Response', {"status": int, "data": Profession}):
+    profession_to_append = profession.model_dump()
+    temp_bd_professions.append(profession_to_append)
+    return {"status": 200, "data": profession}
+
+
+@app.delete("/profession/delete/{profession_id}")
+def  profession_delete(profession_id: int):
+    for i, profession in enumerate(temp_bd_professions):
+        if profession.get("id") == profession_id:
+            temp_bd_professions.pop(i)
+            break
+    return {"status": 201, "message": "deleted"}
+
+
+@app.put("/profession/{profession_id}")
+def profession_update(profession_id: int, profession: Profession) -> List[Profession]:
+    for prof in temp_bd_professions:
+        if prof.get("id") == profession_id:
+            profession_to_append = profession.model_dump()
+            temp_bd_professions.remove(prof)
+            temp_bd_professions.append(profession_to_append)
+    return temp_bd_professions
--- students/K3339/Berdichevskii_Artem/lr_1/Practice_1/models.py
+++ students/K3339/Berdichevskii_Artem/lr_1/Practice_1/models.py
@@ -1,61 +1,31 @@
-import datetime
-from enum import StrEnum
+from enum import Enum
+from typing import Optional, List
 
 from pydantic import BaseModel
 
 
-class Priority(StrEnum):
-    trivial = "trivial"
-    blocker = "blocker"
-    critical = "critical"
-    major = "major"
-    minor = "minor"
+class RaceType(Enum):
+    director = "director"
+    worker = "worker"
+    junior = "junior"
 
 
-class Status(StrEnum):
-    open = "open"
-    in_progress = "in_progress"
-    done = "done"
-
-
-class LinkStatus(StrEnum):
-    is_blocked_by = "is_blocked_by"
-    blocks = "blocks"
-
-
-class Sprint(BaseModel):
+class Profession(BaseModel):
     id: int
     title: str
-    start_at: datetime.datetime
-    end_at: datetime.datetime
+    description: str
 
 
-class Link(BaseModel):
-    task: "Task"
-    status: LinkStatus
-
-
-class Task(BaseModel):
+class Skill(BaseModel):
     id: int
-    summary: str
-    priority: Priority
-    description: str | None = None
-    planned_end_at: datetime.datetime | None = None
-    status: Status
-    created_at: datetime.datetime
-    updated_at: datetime.datetime
-    sprint: Sprint | None = None
+    name: str
+    description: str
 
 
-class TaskWithLinks(Task):
-    linked_tasks: list[Link] | None = []
-
-
-class Message(BaseModel):
-    status: int
-    message: str
-
-
-class Data(BaseModel):
-    status: int
-    data: Task
+class Warrior(BaseModel):
+    id: int
+    race: RaceType
+    name: str
+    level: int
+    profession: Profession
+    skills: Optional[List[Skill]] = []
